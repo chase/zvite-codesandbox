d1d75c15865db4ad123a9707779e19bdcde286ca
Chase Colman <5411+chase@users.noreply.github.com>
2021-08-17 19:50:24 +0800

    Fix Vite HMR Pickup changes to the HMR server's name on any Codesandbox that's backed by a GitHub repo

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ import { ApolloServer } from 'apollo-server-express';
 import { resolvers, context } from './src/resolvers.js';
 import { typeDefs } from './src/schema.js';
 
+const { HOSTNAME } = process.env;
+function getHostFromCodesandboxHostname(host, port = null) {
+  const parts = host.split('sse-sandbox-');
+  if (parts.length === 1) {
+    console.log(`We're not hosted on Codesandbox it seems`);
+    process.exit(1);
+  }
+  const prefix = parts[1];
+  return `${prefix}${port ? `-${port}` : ''}.sse.codesandbox.io`;
+}
+console.log('Vite HMR server:', getHostFromCodesandboxHostname(HOSTNAME, 9000));
+
 async function createViteServer() {
   const server = new ApolloServer({ typeDefs, resolvers, context });
   await server.start();
@@ -14,7 +26,7 @@ async function createViteServer() {
       middlewareMode: 'html',
       hmr: {
         protocol: 'wss',
-        host: 'itp53-9000.sse.codesandbox.io',
+        host: getHostFromCodesandboxHostname(HOSTNAME, 9000),
         port: 9000,
         clientPort: 443
       }
